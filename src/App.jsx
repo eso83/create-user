55301a6ed210b18db46a63c786542fea3fb0d1b0
unknown <eazizi193@gmail.com>
2024-10-03 12:36:59 +0330

    adding login component and check username and password .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
+import LogIn from './components/LogIn'
 import './App.css'
 
 function App() {
-
-  
+const[isLogIn,setIsLogIn]=useState(false);
+  let userName
+  let userPass
+  const handleChangeName=(e)=>{
+    userName=e.target.value
+  }
+  const handleChangePass=(e)=>{
+    userPass=e.target.value
+  }
+  const handleLogeIn=()=>{
+    if (userName==="admin"&& userPass==="admin"){
+      setIsLogIn(true)
+    }
+    else{
+      setIsLogIn(false)
+    }
+  }
   return (
-    <>
-    
-    </>
+  <>
+    {!isLogIn?<LogIn onChangeName ={handleChangeName} onChangePass={handleChangePass}  logInOnClick={handleLogeIn}/>:"Welcome"}
+  </>
   )
 }
 
