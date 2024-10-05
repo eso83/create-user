93785af2529169079dc60b8a65eb516d626a00e3
unknown <eazizi193@gmail.com>
2024-10-05 20:20:31 +0330

    todo list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState,useEffect } from 'react'
 import LogIn from './components/LogIn'
 import './App.css'
+// TODO: make all components arrow function
+// TODO: please setup the prettier for the project
+// TODO: make a context and have useAuth custom hook for it which will return a setAuthenticate and authenticate then use that one also when the app is loading the local storage should be checked there and set the value inside a useEffect
 
 function App() {
-  const [isLogIn,setIsLogin]=useState(localStorage.getItem("isLogIn"))
+  const [isLogIn,setIsLogin]=useState(localStorage.getItem("isLogIn"));
+
 
   let userName
   let userPass
