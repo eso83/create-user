d1a3ae4003d8c4d3fc5a6841e47210c15ef369b7
unknown <eazizi193@gmail.com>
2024-10-12 20:49:09 +0330

    custom hook is maked

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,27 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect,createContext,useContext } from 'react'
 import LogIn from './components/LogIn'
 import './App.css'
 // TODO: make all components arrow function
 // TODO: please setup the prettier for the project
 // TODO: make a context and have useAuth custom hook for it which will return a setAuthenticate and authenticate then use that one also when the app is loading the local storage should be checked there and set the value inside a useEffect
-
-function App() {
-  const [isLogIn,setIsLogin]=useState(localStorage.getItem("isLogIn"));
-
-
-  let userName
-  let userPass
-  const handleChangeName=(e)=>{
-    userName=e.target.value
-  }
-  const handleChangePass=(e)=>{
-    userPass=e.target.value
-  }
-  const handleLogeIn=()=>{
-
-    if (userName==="admin"&& userPass==="admin"){
-      setIsLogin(true)
-      localStorage.setItem("isLogIn",true)  
-      
+// TODO: what is merge and rebase / merge vs rebase
+// TODO: Switch to reducer and create another context for login using reducer 
+export const authenticatedContext= createContext();
+export const useAuth=()=>{
+    const authenticate=useContext(authenticatedContext);
+    if(authenticate===undefined){
+        throw new Error("useAuth must be used with a authenticated")
     }
-    else{
-      setIsLogin(false)
-      localStorage.setItem("isLogIn",false)
-    }
-    
-   
-  }
-  
+    return authenticate;
+}
+
+const App=()=> {
+  const [isAuthenticated, setIsAuthenticated] = useState( localStorage.getItem("isLogIn"));
   return (
   <>
-    {!isLogIn?<LogIn onChangeName ={handleChangeName} onChangePass={handleChangePass}  logInOnClick={handleLogeIn}/>:"Welcome"}
+  <authenticatedContext.Provider value={{isAuthenticated,setIsAuthenticated}}>
+    {!isAuthenticated?<LogIn/>:"you are logIn"}
+  </authenticatedContext.Provider>
   </>
   )
 }
--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,13 +1,40 @@
-const LogIn =({onChangeName,onChangePass,logInOnClick})=>{
+import {  useAuth } from "../App"
+const LogIn =()=>{
+    const authenticate=useAuth()
+    let userName
+    let userPass
+    
+    const handleChangeName=(e)=>{
+      userName=e.target.value
+    }
+    const handleChangePass=(e)=>{
+      userPass=e.target.value
+    }
+    const handleLogeIn=()=>{
+  
+      if(userName==="admin" && userPass==="admin"){
+        authenticate.setIsAuthenticated(true)
+        localStorage.setItem("isLogIn",true)  
+        
+      }
+
+      else{
+        authenticate.setIsAuthenticated(false)
+        localStorage.setItem("isLogIn",false)
+        alert('userName or password is incorrect! ')
+      }
+      
+     
+    }
     return( 
         <> 
         <div className="container">
             <h1>logIn</h1>
             <p>userName</p>
-            <input onChange={onChangeName} type="text" placeholder="UserName"/>
+            <input onChange={handleChangeName} type="text" placeholder="UserName"/>
             <p>password</p>
-            <input onChange={onChangePass} type="password"  placeholder="password"/>
-            <button onClick={logInOnClick}>LogIn</button>
+            <input onChange={handleChangePass} type="password"  placeholder="password"/>
+            <button onClick={handleLogeIn}>LogIn</button>
         </div>
         </> 
     )
